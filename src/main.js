7c3f31f9c4662241c6a56c8932be6abb26b1180f
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-01-12 08:05:50 +0700

    a

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import Navigo from "navigo";
+
+
+const router = new Navigo("/");
+// router.on("/", function(){
+//     console.log('Home Page')
+// });
+// router.resolve();
+router.on({
+    "/": () => {
+        console.log('Trang chủ');
+    },
+    "/about": () => {
+        console.log('About Page');
+    }
+})
